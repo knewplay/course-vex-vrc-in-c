3720a8e121aa233e2fd593f3b542f045493b21ec
Touran Khan <touranm@gmail.com>
2024-05-28 15:21:05 -0400

    Add code snippet

---
+++ lemlib-course/session1/example.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
+void autonomous() {
+    // set position to x:0, y:0, heading:0
+    chassis.setPose(0, 0, 0);
+    // turn to face heading 90 with a very long timeout
+    chassis.turnToHeading(90, 100000);
+}
